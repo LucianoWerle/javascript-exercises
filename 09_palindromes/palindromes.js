bcafb96a2dc939668dd09ed2015c614f37a2cd99
LucianoWerle <lucianowerle@outlook.com>
2024-05-15 14:30:17 -0300

    palindrome solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    //convert word to lowercase.
+    string = string.toLowerCase();
+    //create a new variable with the reverse word.
+    let reverseString = string.split('').reverse().join('');
+    //compare string with reverse string and return true or false.
+    if (string === reverseString){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
